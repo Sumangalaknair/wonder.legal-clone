072f2ba53f2bcade39b4fad85eac5a4470dd4c7e
Varun V <varunvirupaksha321@gmail.com>
2020-09-24 13:27:23 +0530

    removed base layout, added 2 flex cols for main page

---
+++ src/pages/Index.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page class="flex">
+
+    <div class="col left-side">
+      Hello
+    </div>
+
+    <div class="col right-side">
+      Bye
+    </div>
+
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
+
+<style lang="scss">
+.right-side{
+  background-color: $cyan-2;
+}
+</style>
