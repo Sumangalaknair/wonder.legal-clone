d37a7670d577e50660dcb701a82c863f7ff9a1b0
Varun V <varunvirupaksha321@gmail.com>
2020-09-24 13:48:39 +0530

    updated styles

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,16 +2,21 @@
   <q-page class="flex">
     <div class="col left-side q-pa-md">
 
-      <q-input outlined v-model="name" label="Enter the name of the recipient" />
+      <q-input outlined v-model="name" label="Enter the name of the recipient" /> <br>
 
-      <q-input outlined v-model="startDate" label="The contract begins on" />
+      <q-input outlined v-model="startDate" label="The contract begins on" /> <br>
 
-      <q-input outlined v-model="endDate" label="Ending date" />
+      <q-input outlined v-model="endDate" label="Ending date" /> <br>
 
     </div>
 
     <div class="col right-side q-pa-sm">
       <div class="text-center text-h5 text-weight-light">Document Preview</div>
+
+      <div>
+      
+      </div>
+
     </div>
   </q-page>
 </template>
