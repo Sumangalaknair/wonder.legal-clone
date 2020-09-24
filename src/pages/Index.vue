d613a6c7b268400fea4cdba1b603b80860b6c21c
Varun V <varunvirupaksha321@gmail.com>
2020-09-24 13:56:35 +0530

    basic binding of data and displaying it in real time on right column

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,22 +1,28 @@
 <template>
   <q-page class="flex">
     <div class="col left-side q-pa-md">
+      <q-input
+        outlined
+        v-model="name"
+        label="Enter the name of the recipient"
+      />
+      <br />
 
-      <q-input outlined v-model="name" label="Enter the name of the recipient" /> <br>
+      <q-input outlined v-model="startDate" label="The contract begins on" />
+      <br />
 
-      <q-input outlined v-model="startDate" label="The contract begins on" /> <br>
+      <q-input outlined v-model="months" label="Duration in months" />
+      <br />
 
-      <q-input outlined v-model="endDate" label="Ending date" /> <br>
-
+      <q-input outlined v-model="endDate" label="Ending date" /> <br />
     </div>
 
     <div class="col right-side q-pa-sm">
-      <div class="text-center text-h5 text-weight-light">Document Preview</div>
+      <div class="text-center text-h5 text-weight-light">Document Preview</div> <br>
 
       <div>
-      
+        <p>Dear {{ name }}, <br> We are delighted to offer you the position of Software Developer at out company, {{ companyName }} starting from: {{startDate}} for a period of {{ months }} months until {{ endDate }}.</p>
       </div>
-
     </div>
   </q-page>
 </template>
@@ -28,7 +34,9 @@ export default {
     return {
       name: "",
       startDate: "",
+      months: "",
       endDate: "",
+      companyName: ""
     };
   }
 };
