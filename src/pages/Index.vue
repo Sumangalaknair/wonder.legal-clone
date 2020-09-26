8cccaf28cc418120abfda703bbf6e0183b80b7d1
Punith Gowda T <poppunith2001@gmail.com>
2020-09-26 14:23:33 +0530

    preloaader added

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -28,7 +28,12 @@
         <q-input outlined v-model="endDate" label="Ending date" /> <br />
       </q-scroll-area>
     </div>
-
+    <div id="loader-bg" v-if="show" class="preloader">
+      <div id="pulse-loader">
+        <div class="pulse-loader-1"></div>
+        <div class="pulse-loader-2"></div>
+      </div>
+    </div>
     <div class="col right-side q-pa-md">
       <div class="text-center text-h5 text-weight-regular">
         Document Preview
@@ -81,7 +86,8 @@ export default {
       startDate: "",
       months: "",
       endDate: "",
-      companyName: ""
+      companyName: "",
+      show: true,
     };
   },
   methods: {
@@ -101,8 +107,17 @@ export default {
       doc.text("OFFER LETTER", pageCenter, 20, "center");
       doc.text(lines, pageleft, 40, "left"); //see this line
       doc.save(pdfName + ".pdf");
-    }
-  }
+    },
+
+    showToggle() {
+      setTimeout(() => {
+        this.show = false;
+      }, 2000);
+    },
+  },
+  mounted() {
+    if (Boolean(this.show)) this.showToggle();
+  },
 };
 </script>
 
@@ -112,5 +127,124 @@ export default {
 }
 p {
   font-size: 18px;
+}
+#pulse-loader .pulse-loader-1,
+#pulse-loader .pulse-loader-2 {
+  -webkit-border-radius: 1000px;
+  -moz-border-radius: 1000px;
+  -o-border-radius: 1000px;
+  border-radius: 1000px
+}
+
+#pulse-loader {
+  position: fixed;
+  z-index: 20;
+  left: 50%;
+  top: 50%;
+  width: 300px;
+  height: 300px;
+  margin-left: -150px;
+  margin-top: -150px
+}
+
+#pulse-loader {
+  top: 45%
+}
+
+#pulse-loader>div {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  border-width: 20px;
+  border-style: solid;
+  border-color: rgb(255, 235, 57)
+}
+
+#pulse-loader .pulse-loader-1 {
+  -webkit-animation: pulse1 1s .5s ease infinite;
+  -moz-animation: pulse1 1s .5s ease infinite;
+  -ms-animation: pulse1 1s .5s ease infinite;
+  -o-animation: pulse1 1s .5s ease infinite;
+  animation: pulse1 1s .5s ease infinite
+}
+
+#pulse-loader .pulse-loader-2 {
+  -webkit-animation: pulse1 1s ease infinite;
+  -moz-animation: pulse1 1s ease infinite;
+  -ms-animation: pulse1 1s ease infinite;
+  -o-animation: pulse1 1s ease infinite;
+  animation: pulse1 1s ease infinite
+}
+
+@-webkit-keyframes pulse1 {
+  0% {
+    -webkit-transform: scale(0)
+  }
+  40% {
+    -webkit-transform: scale(0.3);
+    opacity: 1
+  }
+  100% {
+    -webkit-transform: scale(1);
+    opacity: 0
+  }
+}
+
+@-moz-keyframes pulse1 {
+  0% {
+    -moz-transform: scale(0)
+  }
+  40% {
+    -moz-transform: scale(0.3);
+    opacity: 1
+  }
+  100% {
+    -moz-transform: scale(1);
+    opacity: 0
+  }
+}
+
+@-o-keyframes pulse1 {
+  0% {
+    -o-transform: scale(0)
+  }
+  40% {
+    -o-transform: scale(0.3);
+    opacity: 1
+  }
+  100% {
+    -o-transform: scale(1);
+    opacity: 0
+  }
+}
+
+@-ms-keyframes pulse1 {
+  0% {
+    -ms-transform: scale(0)
+  }
+  40% {
+    -ms-transform: scale(0.3);
+    opacity: 1
+  }
+  100% {
+    -ms-transform: scale(1);
+    opacity: 0
+  }
+}
+
+@keyframes pulse1 {
+  0% {
+    transform: scale(0)
+  }
+  40% {
+    transform: scale(0.3);
+    opacity: 1
+  }
+  100% {
+    transform: scale(1);
+    opacity: 0
+  }
 }
 </style>
